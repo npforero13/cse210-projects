885f65a0cd8e00fd221b9d545f02384c0ef422a0
npforero13 <115837581+npforero13@users.noreply.github.com>
2024-03-18 18:01:39 -0600

    loops

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string userMagicNum = Console.ReadLine();
+        //int intMagicNum = int.Parse(userMagicNum);
+
+
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 100);
+
+        int guess = 0;
+        while (guess != magicNum)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+            if (guess > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You got it!");
+            }
+        }  while (!(magicNum == guess));
+
+
+        
+
+        
     }
 }
