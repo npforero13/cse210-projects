fb54eaba9dec621c6425c7110935ca191051c6ec
npforero13 <115837581+npforero13@users.noreply.github.com>
2024-04-25 23:24:07 -0600

    finished prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+
+        string letterGrade = "";
+
+        if (number >= 90)
+        {
+             letterGrade = "A";
+        }
+        else if (number >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (number >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (number >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (number < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine("Your grade is " + letterGrade);
+
+        if (letterGrade != "F")
+        {
+            Console.WriteLine("You passed bro.");
+        }
+        else
+        {
+            Console.WriteLine("You failed bro.");
+        }
+
     }
+
+    
 }
