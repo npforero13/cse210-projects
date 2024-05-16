c0efde4d4374c73336f37de9e04ef60218ee8317
npforero13 <115837581+npforero13@users.noreply.github.com>
2024-05-15 22:05:50 -0600

    finished learn02

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(3, 4);
+
+        Console.WriteLine(fraction1.GetFractionString());
+        
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        fraction3.SetNumerator(1);
+        fraction3.SetDenominator(3);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
     }
 }
